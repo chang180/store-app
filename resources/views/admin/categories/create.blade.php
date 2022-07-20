d9c2cc2fe36dfe3124f31eb9c568cedbdf9a90ee
CHANG180-PC\chang <chang180@gmail.com>
2022-07-20 23:37:13 +0800

    add category function

--- resources/views/admin/categories/create.blade.php
+++ resources/views/admin/categories/create.blade.php
@@ -12,25 +12,26 @@
                     class="px-4 py-2 bg-indigo-500 hover:bg-indigo-700 rounded-lg">Category Index</a>
             </div>
             <div class="m-2 p-2 bg-slate-100 rounded">
-                <form>
+                <form method="POST" enctype="multipart/form-data" action="{{ route('admin.categories.store') }}">
+                    @csrf
                     <div class="mb-6">
                         <label for="name" class="mb-2 block text-sm font-medium text-gray-900 dark:text-gray-300">
                             Name</label>
-                        <input type="name" id="email"
+                        <input type="text" id="name" name="name"
                             class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
                             required />
                     </div>
                     <div class="mb-6">
                         <label for="image"
                             class="mb-2 block text-sm font-medium text-gray-900 dark:text-gray-300">Image</label>
-                        <input type="file" id="image"
+                        <input type="file" id="image" name="image"
                             class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
                             required />
                     </div>
                     <div class="mb-6">
                         <label for="description"
                             class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Description</label>
-                        <textarea id="description" rows="4"
+                        <textarea id="description" rows="4" name="description"
                             class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"></textarea>
                     </div>
                     <button type="submit"
